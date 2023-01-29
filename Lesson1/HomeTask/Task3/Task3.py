f0775f8e7ad7f2958193503d8a744f3550c5a779
george <georgij5ptb@gmail.com>
2023-01-29 09:30:49 +0300

    Сделали задание 3

--- Lesson1/HomeTask/Task3/Task3.py
+++ Lesson1/HomeTask/Task3/Task3.py
@@ -8,3 +8,10 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+number = input("Введите ваше число > ")
+number_two = int(number + number)
+number_three = int(number + number + number)
+number = int(number)
+result = number + number_two + number_three
+print(f"{number} + {number_two} + {number_three} = {result}")
