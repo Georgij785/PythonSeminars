5d2b20c83c0908f432d668e497065481764c5be3
george <georgij5ptb@gmail.com>
2023-02-12 09:19:49 +0300

    сделали 3

--- Lesson4/HomeTask4/Task3.py
+++ Lesson4/HomeTask4/Task3.py
@@ -5,4 +5,18 @@
 
 - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 '''
+import random
 
+k = int(input())
+
+number = []
+for i in range(1, 100):
+    number.append(random.randint(1, 100))
+number_final = ""
+a = k
+for item in range(k):
+    number_final += f"{number[item]}x^{a}+"
+    a -= 1
+y = random.randint(0, 100)
+number_final += str(number[y])
+print(number_final)
