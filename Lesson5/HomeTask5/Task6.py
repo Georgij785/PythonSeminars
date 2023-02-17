d22e1234671a6cfbb690e5a584612a7154fd4bd0
george <georgij5ptb@gmail.com>
2023-02-17 22:11:02 +0300

    номер 6

--- Lesson5/HomeTask5/Task6.py
+++ Lesson5/HomeTask5/Task6.py
@@ -6,3 +6,23 @@
 то вывести загаданное число.
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+import random
+def game_binary(x, count=1):
+    if count == 11:
+        print("вы проиграли")
+        return
+    else:
+        y = int(input("введите число > "))
+        if y == x:
+            print("верно, вы выйграли")
+            return
+        elif y > x:
+            print("больше загадонного")
+        else:
+            print("меньше загадонного")
+        count += 1
+        game_binary(x, count)
+
+
+x = random.randint(0, 100)
+game_binary(x)
