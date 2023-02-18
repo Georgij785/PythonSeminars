7919e1365dd05d6adc54596fad1ab8d9f85d277d
george <georgij5ptb@gmail.com>
2023-02-18 17:52:39 +0300

    задание 2

--- Lesson6/HomeTask6/Task2.py
+++ Lesson6/HomeTask6/Task2.py
@@ -2,3 +2,20 @@
 Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше
 заданного минимума и не больше заданного максимума)
 '''
+import random
+
+my_list = [i for i in range(1, 100)]
+indexes = []
+
+
+def borders(numbers_list, min_v, max_v, indexes, c):
+    if c == len(numbers_list):
+        return
+    else:
+        if numbers_list[c] >= min_v and numbers_list[c] <= max_v:
+            indexes.append(c)
+        borders(numbers_list, min_v, max_v, indexes, c + 1)
+
+
+borders(my_list, 10, 15, indexes, 0)
+print(indexes)
