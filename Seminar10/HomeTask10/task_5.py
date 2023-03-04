16e2109cfeaad677c16c43709eddd34d9b886cae
george <georgij5ptb@gmail.com>
2023-03-04 23:37:38 +0300

    5

--- Seminar10/HomeTask10/task_5.py
+++ Seminar10/HomeTask10/task_5.py
@@ -7,3 +7,20 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+import subprocess
+import chardet
+
+ARGS = ['ping', 'yandex.ru']
+YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+for line in YA_PING.stdout:
+    result = chardet.detect(line)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
+
+
+ARGS_1 = ['ping',"youtube.com"]
+YA_PING_1 = subprocess.Popen(ARGS_1, stdout=subprocess.PIPE)
+for line_0 in YA_PING_1.stdout:
+    result_1 = chardet.detect(line_0)
+    line_1 = line_0.decode(result_1['encoding']).encode('utf-8')
+    print(line_1.decode())
