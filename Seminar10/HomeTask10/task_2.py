05dcffd7d643a70022ad9d9f6830372183a0493d
george <georgij5ptb@gmail.com>
2023-03-04 22:25:13 +0300

    2

--- Seminar10/HomeTask10/task_2.py
+++ Seminar10/HomeTask10/task_2.py
@@ -10,3 +10,15 @@
 --- b'class' - используйте маркировку b''
 --- используйте списки и циклы, не дублируйте функции
 """
+a=b"class"
+b=b"function"
+c=b"method"
+
+my_list=list()
+my_list.append(a)
+my_list.append(b)
+my_list.append(c)
+for i in my_list:
+    print(type(i))
+    print(i)
+    print(len(i))
